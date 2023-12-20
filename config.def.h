443f4a1c57edd3df10ff66e216e262f9bef29afa
James-Steve <hamsterofgold@gmail.com>
2023-12-20 15:06:44 +0200

    Slock intial settings

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "jim";
+static const char *group = "jim";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
